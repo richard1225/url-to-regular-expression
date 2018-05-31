031fa982a9cd54999584c25777742a0bff467f53
richard1225 <a770123341@qq.com>
2018-05-31 16:25:20 +0800

    fix: 修复迭代器

--- core/iterate_scheduler.py
+++ core/iterate_scheduler.py
@@ -1,10 +1,17 @@
+#!-*- encoding:utf-8 -*-
+"""
+   迭代控制器
+"""
 import sys
+import time
 import new_cluster as clus
 import verify_coverage as vco
 import extract_pattern as epat
 
 
 def cluster_url():
+    print ">>> clusting..."
+    start = time.time()
     depth = sys.argv[1]
     repeat = sys.argv[2]
     url_file = sys.argv[3]
@@ -16,11 +23,17 @@ def cluster_url():
 
     for url in url_list:
         cluster.insert(pat, url)
-    
+    end = time.time()
+
+    print "clusting cost %s s"%str(int(end-start))
+    print
+
     return cluster.get_repeat()
 
 
 def extract_url(cluster_url):
+    print ">>> extracting url..."
+    start = time.time()
     fname = 'raw_url.txt'
     with open(fname,'w') as f:
         f.write(cluster_url)
@@ -53,17 +66,28 @@ def extract_url(cluster_url):
                             url_list.append(line.strip())
                 else:
                     break
+    
+    end = time.time()
+    print "extract cost %s s"%str(int(end-start))
+    print 
+
     return re_fname
     
 def verify_coverage(url_fname,re_fname):
+    print ">>> verifying url..."
+    start = time.time()
     depth = sys.argv[1]
     repeat = sys.argv[2]
     print '-'*30
     print 'depth=',depth, 'repeat=',repeat
     vco.verify(url_fname,re_fname)
     print '-'*30
+    end = time.time()
+    print "verify cost %s s"%str(int(end-start))
+    print 
 
 if __name__ == '__main__':
     cluster = cluster_url()
     re_fname = extract_url(cluster)
     verify_coverage(sys.argv[3],re_fname)
+    
